6e079acf9bbb5a38d85700d429db4a25e9e45881
ono.rikuto <ono.rikuto@dsic.jp>
2025-07-15 16:42:44 +0900

    トップページの修正

--- src/app/(pages)/top/page.tsx
+++ src/app/(pages)/top/page.tsx
@@ -1,22 +1,17 @@
 import Image from "next/image";
 
-export default async function Page() {
-  const randomNum = Math.floor(Math.random() * 6);
-  const staffList =[
-    "/staff/amy-burns.png",
-    "/staff/balezs-orban.png",
-    "/staff/delba-de-oliveira.png",
-    "/staff/evil-rabbit.png",
-    "/staff/lee-robinson.png",
-    "/staff/michael-novotny.png",
-  ]
-
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string };
+}) {
+  const staffImage = searchParams.staff;
   return (
     <>
       <h1 className="text-xl">TOP</h1>
       <div>
         <Image
-          src={staffList[randomNum]}
+          src={staffImage}
           alt="スタッフ写真"
           width={50}
           height={50}
