47eb53010c70ae5ea22ad59ae8c924edae638b6b
ono.rikuto <ono.rikuto@dsic.jp>
2025-07-15 16:41:03 +0900

    エラーメッセージダイヤログファイルの追加

---
+++ src/app/components/errorMessageDiaolog.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+export default function ErrorMessageDiaolog() {
+  return (
+    <dialog id="showErrorMessage" className="modal">
+      <div role="alert" className="alert alert-error mb-200 w-[550px]">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6 shrink-0 stroke-current"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+        <span>The ID or password is incorrect.</span>
+        <form method="dialog">
+          {/* if there is a button in form, it will close the modal */}
+          <button className="btn">Close</button>
+        </form>
+      </div>
+    </dialog>
+  );
+}
