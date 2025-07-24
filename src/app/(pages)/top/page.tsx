47dd293cac54b19d004e612691469aa3612f1684
ono.rikuto <ono.rikuto@dsic.jp>
2025-07-24 14:37:45 +0900

    トップページ修正

--- src/app/(pages)/top/page.tsx
+++ src/app/(pages)/top/page.tsx
@@ -1,16 +1,34 @@
+import { Loading } from "@/app/components/Loading";
+import React, { Suspense } from "react";
 import Image from "next/image";
-import Parameter from "@/app/components/Parameter";
 
 export default function Page({
   searchParams,
 }: {
   searchParams?: { [key: string]: string | string[] | undefined };
 }) {
-  const staffImage = (searchParams?.staff as string) || "/staff/amy-burns.png";
+  const Parameter = React.lazy(() => import("../../components/Parameter"));
 
+  const staffIcon = (searchParams?.staff as string) || "/staff/amy-burns.png";
+  let staffImage;
+  if (staffIcon === "1") {
+    staffImage = "/staff/amy-burns.png";
+  } else if (staffIcon === "2") {
+    staffImage = "/staff/balazs-orban.png";
+  } else if (staffIcon === "3") {
+    staffImage = "/staff/delba-de-oliveira.png";
+  } else if (staffIcon === "4") {
+    staffImage = "/staff/evil-rabbit.png";
+  } else if (staffIcon === "5") {
+    staffImage = "/staff/lee-robinson.png";
+  } else if (staffIcon === "6") {
+    staffImage = "/staff/michael-novotny.png";
+  } else {
+    staffImage = "/staff/amy-burns.png";
+  }
   return (
     <>
-      <div className="space-y-6">
+      <div className="space-y-6 relative">
         <h1 className="text-xl">TOP</h1>
         <div>
           <Image
@@ -23,7 +41,9 @@ export default function Page({
           {/* For TSX uncomment the commented types below */}
         </div>
         <div>
-          <Parameter />
+          <Suspense fallback={<Loading />}>
+            <Parameter />
+          </Suspense>
         </div>
       </div>
     </>
