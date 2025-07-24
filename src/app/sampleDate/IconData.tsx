ffbd65034f0b72b84b63915e4b5a2c0a0d93ae99
ono.rikuto <ono.rikuto@dsic.jp>
2025-07-24 14:38:51 +0900

    アイコンデータの追加

---
+++ src/app/sampleDate/IconData.tsx
@@ -0,0 +1,38 @@
+export const IconData = [
+  {
+    id: 1,
+    name: "Amy Burns",
+    password: "1",
+    staffImage: "/staff/amy-burns.png",
+  },
+  {
+    id: 2,
+    name: "Balazs-Orban",
+    password: "2",
+    staffImage: "/staff/balazs-orban.png",
+  },
+  {
+    id: 3,
+    name: "Delba De Oliveira",
+    password: "3",
+    staffImage: "/staff/delba-de-oliveira.png",
+  },
+  {
+    id: 4,
+    name: "Evil Rabbit",
+    password: "4",
+    staffImage: "/staff/evil-rabbit.png",
+  },
+  {
+    id: 5,
+    name: "Lee Robinson",
+    password: "5",
+    staffImage: "/staff/lee-robinson.png",
+  },
+  {
+    id: 6,
+    name: "Michael Novotny",
+    password: "6",
+    staffImage: "/staff/michael-novotny.png",
+  },
+];
