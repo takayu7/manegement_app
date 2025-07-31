5877d0c25d7f2e3bf963db41563f3153f0297348
takahashi.yuya <takahashi.yuya@dsic.jp>
2025-07-31 10:29:13 +0900

    route修正

--- src/app/api/purchaseHistory/[userId]/route.ts
+++ src/app/api/purchaseHistory/[userId]/route.ts
@@ -1,11 +1,7 @@
 import { fetchPurchaseHistory } from "@/app/lib/api";
 import { NextResponse } from "next/server";
-import { NextRequest } from "next/server";
 
-export async function GET(
-  req: NextRequest,
-  context: { params: { userId: string } }
-) {
-  const data = await fetchPurchaseHistory(context.params.userId);
+export async function GET(req: Request, { params }: { params: { userId: string } }) {
+  const data = await fetchPurchaseHistory(params.userId);
   return NextResponse.json(data);
 }
