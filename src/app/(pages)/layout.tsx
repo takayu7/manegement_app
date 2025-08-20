3a196c54f82ad6581c5e71858ed7963933253205
takahashi.yuya <takahashi.yuya@dsic.jp>
2025-08-20 14:14:31 +0900

    レイアウトの修正

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -13,10 +13,10 @@ export default function RootLayout({
         <div className="absolute top-0 left-0 z-50 w-full bg-white opacity-80 shadow-md">
           <Header />
         </div>
-        <div className="absolute left-7 top-[85px] flex-none lg:w-64">
+        <div className="absolute md:left-7 top-[80px] md:top-[85px] flex-none w-full md:w-64">
           <SideNav />
         </div>
-        <div className="flex-1 rounded-md mt-32 bg-white overflow-y-auto p-4 md:mt-4 lg:p-12">
+        <div className="flex-1 h-5/6 md:h-11/12 rounded-md mt-24 mb-5 bg-white overflow-y-auto p-4 md:mt-4 lg:p-12">
           {children}
         </div>
       </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({
       <body
         className="antialiased flex h-screen flex-col md:flex-row md:overflow-hidden"
       >
-        <div className="flex-grow bg-gray-200 md:overflow-y-auto md:p-12">
+        <div className="flex-grow bg-gray-200 overflow-hidden md:p-12">
           {children}
         </div>
       </body>
