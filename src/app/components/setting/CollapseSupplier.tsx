319ef93dcc595983eaa266329e5cc3da140a7a4b
ono.rikuto <ono.rikuto@dsic.jp>
2025-08-29 17:04:36 +0900

    settting画面の修正

--- src/app/components/setting/CollapseSupplier.tsx
+++ src/app/components/setting/CollapseSupplier.tsx
@@ -36,7 +36,7 @@ export const CollapseSupplier = async () => {
 
   return (
     <>
-      <div className="bg-base-100 border-base-300 collapse border">
+      <div className="mt-5 bg-base-100 border-base-300 collapse border">
         <input type="checkbox" className="peer" />
         <div className="px-[16px] collapse-title bg-[#6E6B41] text-[30px] text-primary-content text-center peer-checked:bg-[#3F9877] peer-checked:text-secondary-content">
           Supplier
